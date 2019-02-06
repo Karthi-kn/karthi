855e075e77ef75c007ad6d42cb22f358a42c8df2
Karthi-kn <47377227+Karthi-kn@users.noreply.github.com>
2019-02-06 02:07:33 -0800

    Update largest number.java

--- largest number.java
+++ largest number.java
@@ -1,8 +1,8 @@
-public class Largest {
+class Largest {
 
     public static void main(String[] args) {
 
-        int n1 = 1, n2 = 2, n3 = 3;
+        int n1 = 1, n2 = 1, n3 = 4;
 
         if( n1 >= n2 && n1 >= n3)
             System.out.println(n1 + " ");
