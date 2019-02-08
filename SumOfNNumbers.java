b015ead6d33c4d10c312322925f87876201f936e
Karthi-kn <47377227+Karthi-kn@users.noreply.github.com>
2019-02-08 16:39:04 +0530

    Create SumOfNNumbers.java

---
+++ SumOfNNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfNNumbers {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		int number, i, sum = 0;
+		sc = new Scanner(System.in);
+		
+		System.out.println("");
+		number = sc.nextInt();	
+		
+		for(i = 1; i <= number; i++)
+		{
+			sum = sum + i; 
+		}	
+		
+		System.out.println(""+ sum);
+	}
+}
